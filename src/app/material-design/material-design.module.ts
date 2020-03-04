a399f56702429ec1c04f7887906a2c097161f72d
Misael Taveras <macrot29@gmail.com>
2020-03-04 08:48:10 -0400

    feat: add base currency selector

--- src/app/material-design/material-design.module.ts
+++ src/app/material-design/material-design.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from "@angular/core";
 import { MatToolbarModule } from "@angular/material/toolbar";
 import { MatButtonModule } from "@angular/material/button";
 import { MatButtonToggleModule } from "@angular/material/button-toggle";
+import { MatMenuModule } from "@angular/material/menu";
 
 const MaterialModules = [
   MatToolbarModule,
   MatButtonModule,
-  MatButtonToggleModule
+  MatButtonToggleModule,
+  MatMenuModule
 ];
 
 @NgModule({
