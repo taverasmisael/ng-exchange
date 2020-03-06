ed5d559adaea9741f362abc8b00d7074343c0510
Misael Taveras <macrot29@gmail.com>
2020-03-06 00:53:10 -0400

    feat: add ChartsModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { HttpClientModule } from "@angular/common/http";
+import { NoopAnimationsModule } from "@angular/platform-browser/animations";
+
+import { ChartsModule } from "ng2-charts";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
-import { NoopAnimationsModule } from "@angular/platform-browser/animations";
 import { MaterialDesignModule } from "./material-design/material-design.module";
 import { HistoricPageComponent } from "./views/historic-page/historic-page.component";
 import { LatestPageComponent } from "./views/latest-page/latest-page.component";
 import { ComparisonPageComponent } from "./views/comparison-page/comparison-page.component";
 import { MainNavComponent } from "./main-nav/main-nav.component";
-import { FluctuationIconComponent } from './components/fluctuation-icon/fluctuation-icon.component';
+import { FluctuationIconComponent } from "./components/fluctuation-icon/fluctuation-icon.component";
 
 @NgModule({
   declarations: [
@@ -26,7 +28,8 @@ import { FluctuationIconComponent } from './components/fluctuation-icon/fluctuat
     HttpClientModule,
     AppRoutingModule,
     NoopAnimationsModule,
-    MaterialDesignModule
+    MaterialDesignModule,
+    ChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
