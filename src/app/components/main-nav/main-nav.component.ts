2e46f08c0c4aeaf8ba7273f48e73913f3ee12d49
Misael Taveras <macrot29@gmail.com>
2020-03-07 16:54:03 -0400

    feat: add functionality to MainNav currency selector

--- src/app/components/main-nav/main-nav.component.ts
+++ src/app/components/main-nav/main-nav.component.ts
@@ -1,21 +1,30 @@
-import { Component } from '@angular/core';
-import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
-import { Observable } from 'rxjs';
-import { map, shareReplay } from 'rxjs/operators';
+import { Component } from "@angular/core";
+import { BreakpointObserver, Breakpoints } from "@angular/cdk/layout";
+import { Observable } from "rxjs";
+import { map, shareReplay } from "rxjs/operators";
+import { BaseCurrencyService } from "src/app/services/currency/base-currency.service";
 
 @Component({
-  selector: 'app-main-nav',
-  templateUrl: './main-nav.component.html',
-  styleUrls: ['./main-nav.component.scss']
+  selector: "app-main-nav",
+  templateUrl: "./main-nav.component.html",
+  styleUrls: ["./main-nav.component.scss"]
 })
 export class MainNavComponent {
-
-  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+  isHandset$: Observable<boolean> = this.breakpointObserver
+    .observe(Breakpoints.Handset)
     .pipe(
       map(result => result.matches),
       shareReplay()
     );
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  currentCurrency$ = this.baseCurrency.currentCurrency;
 
+  constructor(
+    private breakpointObserver: BreakpointObserver,
+    private baseCurrency: BaseCurrencyService
+  ) {}
+
+  setBaseCurrency(currency: string) {
+    this.baseCurrency.setCurrent(currency);
+  }
 }
