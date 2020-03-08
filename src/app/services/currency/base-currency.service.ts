71e0fcdce1b0c12e38b83f23da066f1d5cfd359d
Misael Taveras <macrot29@gmail.com>
2020-03-08 13:08:17 -0400

    feat: save baseCurrency on localStorage
    a nice touch is now you cannot setCurrency to the same value

--- src/app/services/currency/base-currency.service.ts
+++ src/app/services/currency/base-currency.service.ts
@@ -7,12 +7,19 @@ import { BehaviorSubject } from "rxjs";
 })
 export class BaseCurrencyService {
   readonly currentCurrency = new BehaviorSubject("EUR");
+  private readonly LSKEY = "XO-BASECURRENCY";
   current: string;
   constructor() {
+    if (localStorage.getItem(this.LSKEY)) {
+      this.setCurrent(localStorage.getItem(this.LSKEY));
+    }
     this.currentCurrency.subscribe(currency => (this.current = currency));
   }
 
   setCurrent(currency: string) {
-    this.currentCurrency.next(currency);
+    if (currency !== this.current) {
+      localStorage.setItem(this.LSKEY, currency);
+      this.currentCurrency.next(currency);
+    }
   }
 }
